80bed7548dde68ed0ac5f36cbb630eeb87c3cf19
Samantha Gatt <smfgatt@gmail.com>
2018-08-09 13:18:21 -0400

    Configures entry model

---
+++ Journal/Journal/Model and Controller/Entry.swift
@@ -0,0 +1,24 @@
+//
+//  Entry.swift
+//  Journal
+//
+//  Created by Samantha Gatt on 8/9/18.
+//  Copyright © 2018 Samantha Gatt. All rights reserved.
+//
+
+import Foundation
+
+struct Entry: Codable, Equatable {
+    
+    var title: String
+    var body: String
+    let timestamp: Date
+    let identifier: String
+    
+    init(title: String, body: String, timestamp: Date = Date(), identifier: String = UUID().uuidString) {
+        self.title = title
+        self.body = body
+        self.timestamp = timestamp
+        self.identifier = identifier
+    }
+}
---
+++ Journal/Journal/Model and Controller/EntryController.swift
@@ -0,0 +1,9 @@
+//
+//  EntryController.swift
+//  Journal
+//
+//  Created by Samantha Gatt on 8/9/18.
+//  Copyright © 2018 Samantha Gatt. All rights reserved.
+//
+
+import Foundation
