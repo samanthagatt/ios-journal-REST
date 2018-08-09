12ba40f455c5c57a32c12888bb17a43a3f692031
Samantha Gatt <smfgatt@gmail.com>
2018-08-09 15:14:36 -0400

    Configures entry controller

--- Journal/Journal/Model and Controller/EntryController.swift
+++ Journal/Journal/Model and Controller/EntryController.swift
@@ -7,3 +7,101 @@
 //
 
 import Foundation
+
+class EntryController: Codable {
+    
+    // MARK: - Properties
+    var entries: [Entry] = []
+    static let baseURL = URL(string: "https://samanthagattsjournal.firebaseio.com/")!
+    
+    // MARK: - CRUD
+    
+    func create(title: String, body: String, completion: @escaping (Error?) -> Void) {
+        
+        let entry = Entry(title: title, body: body)
+        
+        put(entry: entry, completion: completion)
+    }
+    
+    func update(entry: Entry, title: String, body: String, completion: @escaping (Error?) -> Void) {
+        
+        var scratch = entry
+        scratch.title = title
+        scratch.body = body
+        
+        // Is this needed?
+//        guard let index = entries.index(of: entry) else { return }
+//        entries[index] = scratch
+        
+        put(entry: scratch, completion: completion)
+    }
+    
+    
+    // MARK: - Networking
+    
+    // Put
+    func put(entry: Entry, completion: @escaping (Error?) -> Void) {
+        
+        let data: Data
+        
+        do {
+            data = try JSONEncoder().encode(entry)
+        } catch {
+            NSLog("Error encoding \(entry.title) entry: \(error)")
+            completion(error)
+            return
+        }
+        
+        let requestURL = EntryController.baseURL
+            .appendingPathComponent(entry.identifier)
+            .appendingPathExtension("json")
+        
+        var request = URLRequest(url: requestURL)
+        request.httpMethod = "PUT"
+        request.httpBody = data
+        
+        URLSession.shared.dataTask(with: request) { (data, _, error) in
+            
+            if let error = error {
+                NSLog("Error pushing data to server: \(error)")
+                completion(error)
+                return
+            }
+            
+            completion(nil)
+            
+        }.resume()
+    }
+    
+    // Fetch
+    func fetch(completion: @escaping (Error?) -> Void) {
+        
+        let requestURL = EntryController.baseURL.appendingPathExtension("json")
+        
+        URLSession.shared.dataTask(with: requestURL) { (data, _, error) in
+            
+            if let error = error {
+                NSLog("Error fetching data from server: \(error)")
+                completion(error)
+                return
+            }
+            
+            guard let data = data else {
+                completion(error)
+                return
+            }
+            
+            do {
+                
+                let entryDicts = try JSONDecoder().decode([String: Entry].self, from: data)
+                let entries = entryDicts.compactMap { $0.value }
+                self.entries = entries.sorted { $0.timestamp > $1.timestamp }
+                
+            } catch {
+                NSLog("Error decoding data: \(error)")
+                completion(error)
+                return
+            }
+        }
+    }
+}
