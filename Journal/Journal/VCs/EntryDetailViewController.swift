d23c452ed90db5d2ea6d9e80e7d4fa034bdc0fb2
Samantha Gatt <smfgatt@gmail.com>
2018-08-09 17:34:54 -0400

    Configures entry detail view controller

--- Journal/Journal/VCs/EntryDetailViewController.swift
+++ Journal/Journal/VCs/EntryDetailViewController.swift
@@ -10,10 +10,27 @@ import UIKit
 
 class EntryDetailViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
         
+        updateViews()
+    }
+    
+    
+    // MARK: - Functions
+    
+    func updateViews() {
+        if isViewLoaded {
+            
+            guard let thisEntry = entry else {
+                navigationItem.title = "Create Entry"
+                return
+            }
+            
+            navigationItem.title = thisEntry.title
+            titleTextField.text = thisEntry.title
+            bodyTextView.text = thisEntry.body
+        }
     }
     
     
@@ -21,12 +38,35 @@ class EntryDetailViewController: UIViewController {
     
     @IBAction func save(_ sender: Any) {
         
+        guard let title = titleTextField.text,
+            let body = bodyTextView.text else { return }
+        
+        if let thisEntry = entry {
+            entryController?.update(entry: thisEntry, title: title, body: body, completion: { (error) in
+                DispatchQueue.main.async {
+                    self.navigationController?.popViewController(animated: true)
+                }
+            })
+        } else {
+            entryController?.create(title: title, body: body, completion: { (error) in
+                DispatchQueue.main.async {
+                    self.navigationController?.popViewController(animated: true)
+                }
+            })
+        }
     }
     
     
     // MARK: - Properties
     
-    var entry: Entry?
+    var entry: Entry? {
+        didSet {
+            // Don't really know if this is ever going to be called on a background thread but better to be safe I guess
+            DispatchQueue.main.async {
+                self.updateViews()
+            }
+        }
+    }
     var entryController: EntryController?
     
     
