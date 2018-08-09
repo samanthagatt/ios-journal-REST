8fed990ec6f6ca3949b6d243c03559505d6650f6
Samantha Gatt <smfgatt@gmail.com>
2018-08-09 12:57:21 -0400

    Configures entry table view and entry cell

---
+++ Journal/Journal/VCs/EntryTableViewCell.swift
@@ -0,0 +1,20 @@
+//
+//  EntryTableViewCell.swift
+//  Journal
+//
+//  Created by Samantha Gatt on 8/9/18.
+//  Copyright © 2018 Samantha Gatt. All rights reserved.
+//
+
+import UIKit
+
+class EntryTableViewCell: UITableViewCell {
+
+    // MARK: - Outlets
+    
+    @IBOutlet weak var titleTextLabel: UILabel!
+    @IBOutlet weak var timestampTextLabel: UILabel!
+    @IBOutlet weak var bodyTextLabel: UILabel!
+    
+
+}
